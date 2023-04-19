7d1ca12b0bd00a2fb1efec3c276a286c0c28970e
BretMorrisGitHub <bret.morris0905@gmail.com>
2023-04-19 11:57:23 -0700

    JavaScript commit

---
+++ Basic_JavaScript_Projects/Project1_Expressions_Alert/JS/main.js
@@ -0,0 +1,6 @@
+window.alert("Hello, World!"); //This line prints 'Hello, World!' in an alert box
+var sent1 = "This is the beginning of the string, "; //This line defines the 'sent1' variable
+var sent2 = "and this is the end of the string."; //This line defines the 'sent2' variable
+document.write(sent1 + sent2); //This line prints two variables, and concatenates them
+
+document.write(152/3); //This line is an example of printing an expression
